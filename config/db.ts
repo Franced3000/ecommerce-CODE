8ad88affda0abea43b3d3177f1e43665ef754337
Franced3000 <franc.spata@gmail.com>
2024-07-17 15:54:02 +0200

    aggiunte associazioni

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,11 @@
 // sync.ts
 import { Dialect, Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import User from '../models/user';
+import Cart from '../models/cart';
+import Product from '../models/product';
+import Order from '../models/order';
+import Category from '../models/category';
 dotenv.config();
 
 const dbName = process.env.DB_NAME as string;
@@ -17,10 +22,12 @@ const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
 
 
 // Associazioni
-// Tavolo.hasMany(Prenotazione, { foreignKey: 'tavoloId' });
-// Prenotazione.belongsTo(Tavolo, { foreignKey: 'tavoloId' });
+Product.belongsTo(Category, { foreignKey: 'categoryId' });
+Category.hasMany(Product, { foreignKey: 'categoryId' });
 
-// Cliente.hasMany(Prenotazione, { foreignKey: 'clienteId' });
-// Prenotazione.belongsTo(Cliente, { foreignKey: 'clienteId' });
+User.hasMany(Order, { foreignKey: 'userId' });
+Order.belongsTo(User, { foreignKey: 'userId' });
+
+Cart.belongsTo(User, { foreignKey: 'userId' });
 
 export default sequelizeConnection;
